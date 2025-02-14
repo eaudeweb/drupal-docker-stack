94751942c5c7993cdf99216c5b88731c20df4851
Cristian Romanescu <cristian@romanescu.ro>
2025-02-14 17:51:22 +0200

    Improve profile management

--- example.start.sh
+++ example.start.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env sh
 
+COMPOSE_FILES="-f compose.yml"
+if [ -f app.yml ]; then
+	COMPOSE_FILES="${COMPOSE_FILES} -f app.yml "
+fi
+echo "\033[0;32mUsing compose files: ${COMPOSE_FILES}\033[0m"
+
+# Start all services
+# PROFILES="--profile=*"
+PROFILES="--profile=varnish --profile=solr --profile=mariadb"
+echo "\033[0;32mUsing profile(s)   : ${PROFILES}\033[0m"
+
 # Example commands
-# Start full stack    - docker compose --profile=* up
-# Start reverse proxy - docker compose --profile=cache up
-# Start database      - docker compose --profile=database up
-# Start Solr search   - docker compose --profile=search up
+# Start reverse proxy - docker compose ${PROFILES} ${COMPOSE_FILES} up -d
+# Start database      - docker compose ${PROFILES} ${COMPOSE_FILES} up -d
+# Start Solr search   - docker compose ${PROFILES} ${COMPOSE_FILES} up -d
+
+# Start stack
+docker compose ${PROFILES} ${COMPOSE_FILES} up -d
